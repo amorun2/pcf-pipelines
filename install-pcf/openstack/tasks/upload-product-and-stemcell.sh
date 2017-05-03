a10850849029f8391f0562939fd373d6f0c5e153
Joey <jmcdice@gmail.com>
2017-05-03 03:39:35 +0000

    Add debugging

--- install-pcf/openstack/tasks/upload-product-and-stemcell.sh
+++ install-pcf/openstack/tasks/upload-product-and-stemcell.sh
@@ -18,6 +18,7 @@ function check_for_opsman() {
   IMG_NAME="$OPS_MGR_IMG_NAME-$VERSION"
 
   echo "Looking for $IMG_NAME in glance."
+  openstack image list
   openstack image list | grep -q $IMG_NAME
   if [ $? == 0 ]; then
     echo "$IMG_NAME is already installed."
