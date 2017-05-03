18cfe06722aabff7f0c2c0b0db60f9c9be949fc5
Joey <jmcdice@gmail.com>
2017-05-03 03:27:58 +0000

    Check for existing image in glance

--- install-pcf/openstack/tasks/upload-product-and-stemcell.sh
+++ install-pcf/openstack/tasks/upload-product-and-stemcell.sh
@@ -16,6 +16,13 @@ function check_for_opsman() {
 
   VERSION=$(echo $OPSMAN_FILE | perl -lane "print \$1 if (/pcf-openstack-(.*?).raw/)")
   IMG_NAME="$OPS_MGR_IMG_NAME-$VERSION"
+
+  openstack image list | grep -q $IMG_NAME
+  if [ $? != 0 ]; then
+    echo "$IMG_NAME is already installed."
+    exit 0
+  fi
+
   echo "Installing: $IMG_NAME"
      
   openstack image create --disk-format qcow2 --container-format bare \
